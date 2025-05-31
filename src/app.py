703a4fb48b3532c532eb7a2a256e6499e85e35e3
Marie-Lois <marieloisajongacham@gmail.com>
2025-05-31 16:00:11 +0100

    Added flask cors

--- src/app.py
+++ src/app.py
@@ -2,12 +2,14 @@ import requests
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
